82ea2c27630078c0bcdbcbf0abcb6e5ada820e73
artofscripting <artofscripting@gmail.com>
2019-03-30 10:33:37 -0400

    Update HA1_String_Generator.py

--- HA1_String_Generator.py
+++ HA1_String_Generator.py
@@ -1,9 +1,11 @@
 import hashlib
+import time
 m = hashlib.md5()
 if len(sys.argv) > 3:
 	ha1String = sys.argv[1] + sys.argv[2] + sys.argv[3]
 	m.update(ha1String)
 	print (m.hexdigest())
 else:
-    blah = 'username:realm:password'
-	print (blah)
+    needArg = 'EXAMPLE "py.exe HA1_String_Generator.py username realm password"'
+    print (needArg)
+    time.sleep(60)
